17827fea8dd86715ef8cd781a4e5bc2a29ea5a33
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-31 05:35:47 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,7 +8,9 @@ def reformat_languages(languages)
     lang_hash.each do |name, data| 
       data.each do |data_name, value| 
         if hash.key?(name) == false 
-          hash
+          hash[name] = {data_name => value, :style => [type]}
+        else 
+          hash[name]
       end 
     end 
   end  
