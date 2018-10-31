25ac03286096e8c3310918c80c2e9f5af7a0d164
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-31 05:32:10 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -16,19 +16,4 @@ def reformat_languages(languages)
   hash[:javascript][:style] = arr 
   hash 
 end 
-=======
-  array = languages.select {|key, value| key}
-    arr = []
-  array.each do |key, value|
-    arr << key 
-    value.each do |k, v| 
-      hash[k] = v 
-      v.each do |k1, v1|
-        binding.pry 
-      end 
-    end 
-  end 
-    
-  binding.pry
-end
->>>>>>> 4cc9d42f65b8036b898aa1caf2f4d282384be99f
+
