55d35262931402bea7c78bf7fa9065429203f63f
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-31 05:30:25 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,7 @@ require "pry"
 def reformat_languages(languages)
   # your code here
   hash = Hash.new 
+<<<<<<< HEAD
   arr = []
   
   languages.each do |key, value|
@@ -15,3 +16,19 @@ def reformat_languages(languages)
   hash[:javascript][:style] = arr 
   hash 
 end 
+=======
+  array = languages.select {|key, value| key}
+    arr = []
+  array.each do |key, value|
+    arr << key 
+    value.each do |k, v| 
+      hash[k] = v 
+      v.each do |k1, v1|
+        binding.pry 
+      end 
+    end 
+  end 
+    
+  binding.pry
+end
+>>>>>>> 4cc9d42f65b8036b898aa1caf2f4d282384be99f
